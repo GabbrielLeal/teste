619759ea712f882cb276394d463ac18dbcacc64a
Gabriel Leal <gabriel.sleal@hotmail.com>
2019-07-23 09:13:57 -0300

    adicionado pergunta sobre o sexo do usuario incluindo solução para pessoas não binarias

--- Program.cs
+++ Program.cs
@@ -7,8 +7,19 @@ namespace teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
-            Console.ReadLine();
+            Console.WriteLine("Are you M for Masc or F for Femmale");
+            string s = Console.ReadLine();
+            if(s == "M" || s == "F"){
+                if(s == "M"){
+                    Console.WriteLine("Hello Boy");
+                }
+                else{
+                    Console.WriteLine("Hello Girl");
+                }
+            }
+            else{
+                Console.WriteLine("Are you member of communit LGBTQ+ ?"); 
+            }
         }
     }
 }
